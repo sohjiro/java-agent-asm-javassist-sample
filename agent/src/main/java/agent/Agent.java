b1fc2c8e0030eeb000b1e800e191f97fc521a2b8
sohjiro <felipe@makingdevs.com>
2015-10-16 16:23:50 -0500

    eliminando comentarios

--- agent/src/main/java/agent/Agent.java
+++ agent/src/main/java/agent/Agent.java
@@ -21,17 +21,13 @@ public class Agent {
     inst.addTransformer(new ClassFileTransformer() {
       @Override
       public byte[] transform(ClassLoader classLoader, String s, Class<?> aClass, ProtectionDomain protectionDomain, byte[] bytes) throws IllegalClassFormatException {
-        // if ("javax/servlet/http/HttpServlet".equals(s)) {
         if ("org/springframework/web/servlet/DispatcherServlet".equals(s)) {
           try {
             ClassPool cp = ClassPool.getDefault();
             cp.insertClassPath(new LoaderClassPath(classLoader));
-            // CtClass cc = cp.get("javax.servlet.http.HttpServlet");
-            // CtMethod m = cc.getDeclaredMethod("doGet");
             CtClass cc = cp.get("org.springframework.web.servlet.DispatcherServlet");
             CtMethod m = cc.getDeclaredMethod("doDispatch");
             System.out.println("method : " + m);
-            // m.insertBefore("{ System.out.println(\"EMBRACE ME, AS YOUR KING AND AS YOUR GOD!!!!!!!!!!!!!!!!!!!! MOTHER FOCAS!!!!!!!\"); }");
             m.insertBefore("{ java.util.Enumeration params = $1.getParameterNames(); while(params.hasMoreElements()) { String paramName = (String) params.nextElement(); System.out.println(paramName + \" : \" + $1.getParameter(paramName)); } }");
             byte[] byteCode = cc.toBytecode();
             cc.detach();
