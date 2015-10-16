f4057518391e37597bb83165648a2f5b140d28f1
sohjiro <felipe@makingdevs.com>
2015-10-16 16:14:18 -0500

    interviniendo el DispatcherServlet

--- agent/src/main/java/agent/Agent.java
+++ agent/src/main/java/agent/Agent.java
@@ -9,8 +9,11 @@ import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
 import javassist.ClassPool;
+import javassist.LoaderClassPath;
 import javassist.CtClass;
 import javassist.CtMethod;
+
+import java.lang.*;
 
 public class Agent {
 
@@ -18,21 +21,18 @@ public class Agent {
     inst.addTransformer(new ClassFileTransformer() {
       @Override
       public byte[] transform(ClassLoader classLoader, String s, Class<?> aClass, ProtectionDomain protectionDomain, byte[] bytes) throws IllegalClassFormatException {
-
-        if ("other/Stuff".equals(s)) {
-          // ASM Code
-          // ClassReader reader = new ClassReader(bytes);
-          // ClassWriter writer = new ClassWriter(reader, 0);
-          // ClassPrinter visitor = new ClassPrinter(writer);
-          // reader.accept(visitor, 0);
-          // return writer.toByteArray();
-
-          // Javassist
+        // if ("javax/servlet/http/HttpServlet".equals(s)) {
+        if ("org/springframework/web/servlet/DispatcherServlet".equals(s)) {
           try {
             ClassPool cp = ClassPool.getDefault();
-            CtClass cc = cp.get("other.Stuff");
-            CtMethod m = cc.getDeclaredMethod("runWithParams");
-            m.insertBefore("{ System.out.println($1 + ',' + $2); }");
+            cp.insertClassPath(new LoaderClassPath(classLoader));
+            // CtClass cc = cp.get("javax.servlet.http.HttpServlet");
+            // CtMethod m = cc.getDeclaredMethod("doGet");
+            CtClass cc = cp.get("org.springframework.web.servlet.DispatcherServlet");
+            CtMethod m = cc.getDeclaredMethod("doDispatch");
+            System.out.println("method : " + m);
+            // m.insertBefore("{ System.out.println(\"EMBRACE ME, AS YOUR KING AND AS YOUR GOD!!!!!!!!!!!!!!!!!!!! MOTHER FOCAS!!!!!!!\"); }");
+            m.insertBefore("{ java.util.Enumeration params = $1.getParameterNames(); while(params.hasMoreElements()) { String paramName = (String) params.nextElement(); System.out.println(paramName + \" : \" + $1.getParameter(paramName)); } }");
             byte[] byteCode = cc.toBytecode();
             cc.detach();
             return byteCode;
